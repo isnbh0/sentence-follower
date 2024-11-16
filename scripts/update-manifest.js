cb2432f54ec5f62f0eae2056b7d58b71466b9d20
John Choi <isnbh0@gmail.com>
2024-11-16 16:29:39 +0900

    update manifest with package version

---
+++ scripts/update-manifest.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+// Read package.json to get the new version
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const newVersion = packageJson.version;
+
+// Read and update manifest.json
+const manifestPath = 'manifest.json';
+const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+manifest.version = newVersion;
+
+// Write the updated manifest.json
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+
+console.log(`Updated manifest.json to version ${newVersion}`);
